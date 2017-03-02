e0560a622d69e6a10290c29c8c2560714ae3606e
BykovaDaria <bykova.ds@gmail.com>
2017-03-02 12:55:35 +0300

    Was added add to reserv partition from outer replica

--- Server/src/main/java/ru/dins/kafka/synchronization/QuoteOuterSynchronizer.java
+++ Server/src/main/java/ru/dins/kafka/synchronization/QuoteOuterSynchronizer.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 import ru.dins.kafka.consumer.QuoteConsumer;
+import ru.dins.kafka.producer.QuoteProducer;
 import ru.dins.web.model.quote.Quote;
 import ru.dins.web.persistence.QuoteRepository;
 
@@ -23,6 +24,9 @@ public class QuoteOuterSynchronizer implements Runnable{
 
     @Autowired @Qualifier("outerReplicaTopicConsumerFromFile")
     private QuoteConsumer consumer;
+
+    @Autowired
+    private QuoteProducer producer;
 
     private AtomicBoolean closed = new AtomicBoolean(false);
 
@@ -43,7 +47,13 @@ public class QuoteOuterSynchronizer implements Runnable{
                 if (isSuccesConnection2DB){
                     List<Quote> quotes = consumer.readQuotesFromQueue();
                     if (quotes != null){
-                        repository.addQuotes(quotes);
+                        for (Quote quote : quotes) {
+                            try{
+                                repository.addQuote(quote);
+                            } catch (Exception e){
+                                producer.addQuote2ReservePartitionLocalTopic(quote);
+                            }
+                        }
                     }
                 }
             }
